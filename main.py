8471563a4fc1679c70410c03e5612a40ba0c29ff
José Cabarcas <132719140+Josefct439@users.noreply.github.com>
2023-10-06 18:03:49 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.channel.send("Hi!")
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+    
+
+
+bot.run("MTE1NzQ0Nzg4MjQ3NjA5MzU3MQ.GsydUa.IFwYIFbfB2g1pBOpr_RDqtzQciuQxGlJliFLxg")
